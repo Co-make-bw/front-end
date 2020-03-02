43da2bcf15cf1e21d396b061f8722bdac428f9d3
Eddie Madrigal <edmadrigal@yahoo.com>
2020-03-02 15:39:07 -0800

    footer added

--- co-make/src/App.js
+++ co-make/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './components/Navbar';
 import Default from './components/Default';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Nav />
       <Default />
+      <Footer />
     </div>
   );
 }
