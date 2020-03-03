9039fdc3df4f2fbb56e7169ea9f4c25e71e2cb7d
Natalie Davis <nataliedavis@Natalies-MacBook-Pro.local>
2020-03-03 13:14:21 -0600

    skeleton LocalIssues and Issue Card

--- co-make/src/App.js
+++ co-make/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './components/Navbar';
 import Default from './components/Default';
+import LocalIssues from './components/Issues/LocalIssues';
 
 function App() {
   return (
     <div>
       <Nav />
       <Default />
+      <LocalIssues/>
     </div>
   );
 }
--- co-make/src/components/Issues/LocalIssues.js
+++ co-make/src/components/Issues/LocalIssues.js
@@ -1,9 +1,34 @@
 import React from 'react';
-
+import Issue from './Issue';
+const issues = [
+    {
+        id: 16,
+        title: "No complaints",
+        description: "I just came here to just say that the fried chicken at the Puritan Backroom is delicious!",
+        location: "Manchester",
+        upvoted: 0,
+        created_at: "2020-03-03T15:42:35.153Z",
+        updated_at: "2020-03-03T15:42:35.153Z",
+        user_id: 3,
+        state_id: 29
+    },
+    {
+        id: 17,
+        title: "Bicycles",
+        description: "There needs to be more bicycle lanes downtown. I am too affraid to ride because cars never give me enough room",
+        location: "Manchester",
+        upvotes: 0,
+        created_at: "2020-03-03T15:42:35.153Z",
+        updated_at: "2020-03-03T15:42:35.153Z",
+        user_id: 3,
+        state_id: 29
+    },
+]
 const LocalIssues = () => {
 	return (
 		<div>
-			<h1>LocalIssues</h1>
+			{issues.map(issue => <Issue key={issue.id} issue={issue}/>)}
+			
 		</div>
 	);
 };
