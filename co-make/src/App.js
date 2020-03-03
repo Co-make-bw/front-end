ffaf45c20f30702ad961b32dec996eff7089e339
Natalie Davis <nataliedavis@Natalies-MacBook-Pro.local>
2020-03-03 16:16:14 -0600

    cleaned up app

--- co-make/src/App.js
+++ co-make/src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import Nav from './components/Navbar';
 import Default from './components/Default';
-import LocalIssues from './components/Issues/LocalIssues';
-import UserIssues from './components/Issues/UserIssues';
 
 function App() {
   return (
     <div>
       <Nav />
       <Default />
-      {/* uncomment to see the cards as they are currently rendering */}
-      {/* <LocalIssues/>
-      <UserIssues/> */}
     </div>
   );
 }
